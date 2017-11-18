01ad9c1fcb3aa52603aaacf59d493f4d473c4fc3
Pamela Frieze <pfrieze@gmail.com>
2017-11-18 21:35:33 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,6 +11,7 @@ def add_item(title, price, quantity=1)
   self.total += price * quantity
   quantity.times do
 end
+items << title 
 end
 
 def apply_discount
@@ -19,10 +20,6 @@ def apply_discount
   "After the discount, the total comes to $#{self.total}."
 else "There is no discount to apply."
 end
-end
-
-def items
-  @items << title
 end
 
 def void_last_transaction
