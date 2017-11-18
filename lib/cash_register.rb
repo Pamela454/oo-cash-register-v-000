7d70cec63dd62c9ac6af56d3f67441795f4a22f2
Pamela Frieze <pfrieze@gmail.com>
2017-11-18 21:30:46 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ def add_item(title, price, quantity=1)
 end
 end
 
-def apply_discount(discount)
+def apply_discount
   if discount != 0
   self.total = (total * ((100.0 - discount.to_f)/100)).to_i
   "After the discount, the total comes to #{self.total}."
